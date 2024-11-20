5b16631a13583a59fd5e717123bc10cea3e186c6
kaplego <70806521+kaplego@users.noreply.github.com>
2024-11-20 16:39:14 +0000

    Début header

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <link rel="stylesheet" href="/assets/css/style.css">
+    </head>
+    <body>
+        @yield('body')
+    </body>
+</html>
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,3 @@
+<header>
+    <img src="/assets/images/vinotrip.png" alt="" class="logo">
+</header>
