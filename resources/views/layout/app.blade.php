812237f8cd64917ac54c2054edeae9b1eb7663b7
kaplego <70806521+kaplego@users.noreply.github.com>
2024-11-29 15:43:49 +0000

    CookieBot

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,18 +1,25 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>@yield('title', 'VinoTrip')</title>
-        <link rel="stylesheet" href="/assets/css/style.css">
-        @yield('head')
-    </head>
-    <body>
-        @yield('body')
 
-        <script src="https://unpkg.com/lucide@latest"></script>
-        <script src="/assets/js/main.js" defer></script>
-        @yield('scripts')
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title', 'VinoTrip')</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/cookies.css">
+
+    <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="b446234c-b83c-4bb5-ace9-c9241e79f49b" data-blockingmode="auto" type="text/javascript"></script>
+
+    @yield('head')
+</head>
+<body>
+    @yield('body')
+
+    <script src="https://unpkg.com/lucide@latest"></script>
+    <script src="/assets/js/main.js" defer></script>
+    <script src="/assets/js/cookies.js" defer></script>
+    @yield('scripts')
+</body>
+
 </html>
